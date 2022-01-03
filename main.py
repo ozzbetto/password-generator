07f2282aa4fd646c4449069fb950718b2f42067b
ozzbetto <segovia.al@gmail.com>
2022-01-02 22:15:31 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+print("Password Generator Program")
+
+length = int(input('\nEnter the length of the password: '))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+temp = random.sample(all, length)
+
+password = "".join(temp)
+
+print(password)
