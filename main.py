4975211f46e217cf3f756de0f40750eed26687fb
Betto Rodriguez <segovia.al@gmail.com>
2023-09-14 15:05:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import random
 import string
 
-print("Password generator Program.")
-print("===========================")
+def generate_password(length):
+    """
+    Genera una contraseña aleatoria de la longitud especificada.
+    
+    Args:
+    length (int): La longitud de la contraseña a generar.
 
-length = int(input('\nEnter the length of the password: '))
+    Returns:
+    str: Una cadena que representa la contraseña generada.
+    """
+    if length <= 0:
+        return "La longitud de la contraseña debe ser mayor que cero"
 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-num = string.digits
-symbols = string.punctuation
+    character_pool = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.sample(character_pool, length))
+    
+    return password
 
-all = lower + upper + num + symbols
+def main():
+    print("Password Generator Program.")
+    print("===========================")
 
-temp = random.sample(all, length)
+    try:
+        length = int(input('\nEnter the length of the password: '))
+        password = generate_password(length)
+        print(password)
+    except ValueError:
+        print("Por favor, ingresa un número válido.")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
 
-password = "".join(temp)
-
-print(password)
+if __name__ == "__main__":
+    main()
